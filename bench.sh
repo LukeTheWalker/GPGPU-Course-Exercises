1b37a668537fa7c00dc5e1ce4da50c1d1c52979e
LukeTheWalker <lucag2001@gmail.com>
2022-12-02 16:25:04 +0100

    feat: added proper local memory managment in vectorialed smoothing

--- bench.sh
+++ bench.sh
@@ -4,7 +4,7 @@
 nels=$1
 
 # List of programs to run
-declare -a programs=("vecsmooth.out" "vecsmooth_vec.out" "vecsmooth_lmem.out" "vecsmooth_lmem_vec.out")
+declare -a programs=("vecsmooth.out" "vecsmooth_vec.out" "vecsmooth_lmem.out" "vecsmooth_lmem_vec.out" "vecsmooth_lmem_vec_optimized.out")
 
 # Make each file and run it
 for program in "${programs[@]}"
